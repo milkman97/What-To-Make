8c2ac0bdcb7e11f17a191339232707f27831b446
Tristan Crowson <tcrowson97@gmail.com>
2016-04-13 09:43:05 -0500

    fixed length issue

--- js/apiTest.js
+++ js/apiTest.js
@@ -6,10 +6,44 @@ $('#send').click(
         }
         var url = "http://food2fork.com/api/search?key=315107456e4b02d7abecd295c694c6e6&q=" + list;
         $.getJSON(url, function (data) {
-            console.log(data);
+            console.log(data.recipes.length);
 
-            if (jQuery.isEmptyObject(data)) {
+            if (data.recipes.length === 0) {
 
+            }else if(data.recipes.length === 1){
+                document.getElementById("title1").innerHTML = data.recipes[0].title;
+                
+                $("#picplaceholder1").attr("src", data.recipes[0].image_url);
+                
+                $("#linktorecipe1").attr("href", data.recipes[0].source_url);
+               
+                document.getElementById("publisher1").innerHTML = "Publisher: " + data.recipes[0].publisher;
+                
+                $("#publisherurl1").attr("href", data.recipes[0].publisher_url);
+                
+                document.getElementById("rank1").innerHTML = "Social Rankings " + data.recipes[0].social_rank;
+                
+                
+            }
+            else if(data.recipes.length === 2){
+                document.getElementById("title1").innerHTML = data.recipes[0].title;
+                document.getElementById("title2").innerHTML = data.recipes[1].title;
+                
+                $("#picplaceholder1").attr("src", data.recipes[0].image_url);
+                $("#picplaceholder2").attr("src", data.recipes[1].image_url);
+                
+                $("#linktorecipe1").attr("href", data.recipes[0].source_url);
+                $("#linktorecipe2").attr("href", data.recipes[1].source_url);
+                
+                document.getElementById("publisher1").innerHTML = "Publisher: " + data.recipes[0].publisher;
+                document.getElementById("publisher2").innerHTML = "Publisher: " + data.recipes[1].publisher;
+                
+                $("#publisherurl1").attr("href", data.recipes[0].publisher_url);
+                $("#publisherurl2").attr("href", data.recipes[1].publisher_url);
+                
+                document.getElementById("rank1").innerHTML = "Social Rankings " + data.recipes[0].social_rank;
+                document.getElementById("rank2").innerHTML = "Social Rankings " + data.recipes[1].social_rank;
+                
             } else {
                 document.getElementById("title1").innerHTML = data.recipes[0].title;
                 document.getElementById("title2").innerHTML = data.recipes[1].title;
