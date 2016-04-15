32d853568c197904bb2b9367f4a495aabb5da5fa
Tristan Crowson <tcrowson97@gmail.com>
2016-04-15 14:00:48 -0500

    put cards in their own file to clean up code

---
+++ php/cards.php
@@ -0,0 +1,53 @@
+<div class="row">
+    <div class="col s12 m4">
+        <div class="card animated slideInUp">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="images/foodplaceholder.jpeg" id="picplaceholder1">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4" id="title1">Hallmark Of Burgers<i class="material-icons right">more_vert</i></span>
+                <p><a href="#" target="_blank" id="linktorecipe1">Make This!</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4" id="title1"><i class="material-icons right">close</i></span>
+                <h5 id="publisher1"></h5>
+                <p><a href="#" target="_blank" id="publisherurl1">Visit Them!</a></p>
+                <h6 id="rank1"></h6>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m4">
+        <div class="card animated slideInUp">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="images/burritoplaceholder.jpg" id="picplaceholder2">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4" id="title2">Hallmark Of Burritos<i class="material-icons right">more_vert</i></span>
+                <p><a href="#" target="_blank" id="linktorecipe2">Make This!</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4" id="title2"><i class="material-icons right">close</i></span>
+                <h5 id="publisher2"></h5>
+                <p><a href="#" target="_blank" id="publisherurl2">Visit Them!</a></p>
+                <h6 id="rank2"></h6>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m4">
+        <div class="card animated slideInUp">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="images/pizza.jpg" id="picplaceholder3">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4" id="title3">Hallmark Of Pizza<i class="material-icons right">more_vert</i></span>
+                <p><a href="#" target="_blank" id="linktorecipe3">Make This!</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4" id="title3"><i class="material-icons right">close</i></span>
+                <h5 id="publisher3"></h5>
+                <p><a href="#" target="_blank" id="publisherurl3">Visit Them!</a></p>
+                <h6 id="rank3"></h6>
+            </div>
+        </div>
+    </div>
+</div>
