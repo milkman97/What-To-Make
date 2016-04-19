bd6eff6264ade1220c65222feb3450acfcfcb355
Tristan Crowson <tcrowson97@gmail.com>
2016-04-19 16:57:46 -0500

    favicon

--- php/header.php
+++ php/header.php
@@ -1,5 +1,4 @@
 <head>
-    
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
@@ -8,7 +7,7 @@
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/png" href="/images/myfavicon.png">
     <title>MakeyMit</title>
 </head>
 <nav class="orange darken-2" role="navigation">
