c18804e031d78f07c6cf29c19d1f4a505ffd8910
Tristan Crowson <tcrowson97@gmail.com>
2016-04-19 17:02:03 -0500

    favicon

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/png" href="../images/myfavicon.png">
+    <link rel="icon" type="image/png" href="/images/myfavicon.png">
     <title>MakeyMit</title>
 </head>
 <nav class="orange darken-2" role="navigation">
