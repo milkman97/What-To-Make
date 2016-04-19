6bd867a403f0346bc2e840d2a36529df3b3a0884
Tristan Crowson <tcrowson97@gmail.com>
2016-04-18 19:21:54 -0500

    added glyphacon

--- php/header.php
+++ php/header.php
@@ -7,7 +7,8 @@
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>What to Make</title>
+    <link rel="shortcut icon" href="images/glyphacon.ico" type="image/x-icon">
+    <title>MakeyMit</title>
 </head>
 <nav class="orange darken-2" role="navigation">
     <div class="nav-wrapper">
