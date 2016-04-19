223b43409d69a0199e64ca7c48104f3dde767bc7
Tristan Crowson <tcrowson97@gmail.com>
2016-04-19 16:44:28 -0500

    favicon

--- php/header.php
+++ php/header.php
@@ -8,7 +8,7 @@
     <link type="text/css" rel="stylesheet" href="css/style.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="/images/favicon.ico?" type="image/x-icon">
+    <link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
     <title>MakeyMit</title>
 </head>
 <nav class="orange darken-2" role="navigation">
