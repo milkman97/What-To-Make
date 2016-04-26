d2b10ae318d68171e864106b730a78d633ab0050
Tristan Crowson <tcrowson97@gmail.com>
2016-04-26 18:17:55 -0500

    added 3 more recipe cards

--- js/apiTest.js
+++ js/apiTest.js
@@ -48,7 +48,7 @@ $('#send').click(
                 document.getElementById("rank1").innerHTML = "Social Rankings " + data.recipes[0].social_rank;
                 document.getElementById("rank2").innerHTML = "Social Rankings " + data.recipes[1].social_rank;
 
-            } else {
+            } else if (data.recipes.length === 3) {
                 document.getElementById("title1").innerHTML = data.recipes[0].title;
                 document.getElementById("title2").innerHTML = data.recipes[1].title;
                 document.getElementById("title3").innerHTML = data.recipes[2].title;
@@ -72,6 +72,114 @@ $('#send').click(
                 document.getElementById("rank1").innerHTML = "Social Rankings " + data.recipes[0].social_rank;
                 document.getElementById("rank2").innerHTML = "Social Rankings " + data.recipes[1].social_rank;
                 document.getElementById("rank3").innerHTML = "Social Rankings " + data.recipes[2].social_rank;
+            }else if(data.recipes.length === 4){
+                document.getElementById("title1").innerHTML = data.recipes[0].title;
+                document.getElementById("title2").innerHTML = data.recipes[1].title;
+                document.getElementById("title3").innerHTML = data.recipes[2].title;
+                document.getElementById("title4").innerHTML = data.recipes[3].title;
+
+                $("#picplaceholder1").attr("src", data.recipes[0].image_url);
+                $("#picplaceholder2").attr("src", data.recipes[1].image_url);
+                $("#picplaceholder3").attr("src", data.recipes[2].image_url);
+                $("#picplaceholder4").attr("src", data.recipes[3].image_url);
+
+                $("#linktorecipe1").attr("href", data.recipes[0].source_url);
+                $("#linktorecipe2").attr("href", data.recipes[1].source_url);
+                $("#linktorecipe3").attr("href", data.recipes[2].source_url);
+                $("#linktorecipe4").attr("href", data.recipes[3].source_url);
+
+                document.getElementById("publisher1").innerHTML = "Publisher: " + data.recipes[0].publisher;
+                document.getElementById("publisher2").innerHTML = "Publisher: " + data.recipes[1].publisher;
+                document.getElementById("publisher3").innerHTML = "Publisher: " + data.recipes[2].publisher;
+                document.getElementById("publisher4").innerHTML = "Publisher: " + data.recipes[3].publisher;
+
+                $("#publisherurl1").attr("href", data.recipes[0].publisher_url);
+                $("#publisherurl2").attr("href", data.recipes[1].publisher_url);
+                $("#publisherurl3").attr("href", data.recipes[2].publisher_url);
+                $("#publisherurl4").attr("href", data.recipes[3].publisher_url);
+
+                document.getElementById("rank1").innerHTML = "Social Rankings " + data.recipes[0].social_rank;
+                document.getElementById("rank2").innerHTML = "Social Rankings " + data.recipes[1].social_rank;
+                document.getElementById("rank3").innerHTML = "Social Rankings " + data.recipes[2].social_rank;
+                document.getElementById("rank4").innerHTML = "Social Rankings " + data.recipes[3].social_rank;
+            }else if(data.recipes.length === 5){
+                document.getElementById("title1").innerHTML = data.recipes[0].title;
+                document.getElementById("title2").innerHTML = data.recipes[1].title;
+                document.getElementById("title3").innerHTML = data.recipes[2].title;
+                document.getElementById("title4").innerHTML = data.recipes[3].title;
+                document.getElementById("title5").innerHTML = data.recipes[4].title;
+
+                $("#picplaceholder1").attr("src", data.recipes[0].image_url);
+                $("#picplaceholder2").attr("src", data.recipes[1].image_url);
+                $("#picplaceholder3").attr("src", data.recipes[2].image_url);
+                $("#picplaceholder4").attr("src", data.recipes[3].image_url);
+                $("#picplaceholder5").attr("src", data.recipes[4].image_url);
+
+                $("#linktorecipe1").attr("href", data.recipes[0].source_url);
+                $("#linktorecipe2").attr("href", data.recipes[1].source_url);
+                $("#linktorecipe3").attr("href", data.recipes[2].source_url);
+                $("#linktorecipe4").attr("href", data.recipes[3].source_url);
+                $("#linktorecipe5").attr("href", data.recipes[4].source_url);
+
+                document.getElementById("publisher1").innerHTML = "Publisher: " + data.recipes[0].publisher;
+                document.getElementById("publisher2").innerHTML = "Publisher: " + data.recipes[1].publisher;
+                document.getElementById("publisher3").innerHTML = "Publisher: " + data.recipes[2].publisher;
+                document.getElementById("publisher4").innerHTML = "Publisher: " + data.recipes[3].publisher;
+                document.getElementById("publisher5").innerHTML = "Publisher: " + data.recipes[4].publisher;
+
+                $("#publisherurl1").attr("href", data.recipes[0].publisher_url);
+                $("#publisherurl2").attr("href", data.recipes[1].publisher_url);
+                $("#publisherurl3").attr("href", data.recipes[2].publisher_url);
+                $("#publisherurl4").attr("href", data.recipes[3].publisher_url);
+                $("#publisherurl5").attr("href", data.recipes[4].publisher_url);
+
+                document.getElementById("rank1").innerHTML = "Social Rankings " + data.recipes[0].social_rank;
+                document.getElementById("rank2").innerHTML = "Social Rankings " + data.recipes[1].social_rank;
+                document.getElementById("rank3").innerHTML = "Social Rankings " + data.recipes[2].social_rank;
+                document.getElementById("rank4").innerHTML = "Social Rankings " + data.recipes[3].social_rank;
+                document.getElementById("rank5").innerHTML = "Social Rankings " + data.recipes[4].social_rank;
+            }else{
+                document.getElementById("title1").innerHTML = data.recipes[0].title;
+                document.getElementById("title2").innerHTML = data.recipes[1].title;
+                document.getElementById("title3").innerHTML = data.recipes[2].title;
+                document.getElementById("title4").innerHTML = data.recipes[3].title;
+                document.getElementById("title5").innerHTML = data.recipes[4].title;
+                document.getElementById("title6").innerHTML = data.recipes[5].title;
+
+                $("#picplaceholder1").attr("src", data.recipes[0].image_url);
+                $("#picplaceholder2").attr("src", data.recipes[1].image_url);
+                $("#picplaceholder3").attr("src", data.recipes[2].image_url);
+                $("#picplaceholder4").attr("src", data.recipes[3].image_url);
+                $("#picplaceholder5").attr("src", data.recipes[4].image_url);
+                $("#picplaceholder6").attr("src", data.recipes[5].image_url);
+
+                $("#linktorecipe1").attr("href", data.recipes[0].source_url);
+                $("#linktorecipe2").attr("href", data.recipes[1].source_url);
+                $("#linktorecipe3").attr("href", data.recipes[2].source_url);
+                $("#linktorecipe4").attr("href", data.recipes[3].source_url);
+                $("#linktorecipe5").attr("href", data.recipes[4].source_url);
+                $("#linktorecipe6").attr("href", data.recipes[5].source_url);
+
+                document.getElementById("publisher1").innerHTML = "Publisher: " + data.recipes[0].publisher;
+                document.getElementById("publisher2").innerHTML = "Publisher: " + data.recipes[1].publisher;
+                document.getElementById("publisher3").innerHTML = "Publisher: " + data.recipes[2].publisher;
+                document.getElementById("publisher4").innerHTML = "Publisher: " + data.recipes[3].publisher;
+                document.getElementById("publisher5").innerHTML = "Publisher: " + data.recipes[4].publisher;
+                document.getElementById("publisher6").innerHTML = "Publisher: " + data.recipes[5].publisher;
+
+                $("#publisherurl1").attr("href", data.recipes[0].publisher_url);
+                $("#publisherurl2").attr("href", data.recipes[1].publisher_url);
+                $("#publisherurl3").attr("href", data.recipes[2].publisher_url);
+                $("#publisherurl4").attr("href", data.recipes[3].publisher_url);
+                $("#publisherurl5").attr("href", data.recipes[4].publisher_url);
+                $("#publisherurl6").attr("href", data.recipes[5].publisher_url);
+
+                document.getElementById("rank1").innerHTML = "Social Rankings " + data.recipes[0].social_rank;
+                document.getElementById("rank2").innerHTML = "Social Rankings " + data.recipes[1].social_rank;
+                document.getElementById("rank3").innerHTML = "Social Rankings " + data.recipes[2].social_rank;
+                document.getElementById("rank4").innerHTML = "Social Rankings " + data.recipes[3].social_rank;
+                document.getElementById("rank5").innerHTML = "Social Rankings " + data.recipes[4].social_rank;
+                document.getElementById("rank6").innerHTML = "Social Rankings " + data.recipes[5].social_rank;
             }
 
         }).fail(function() {
--- php/cards.php
+++ php/cards.php
@@ -51,3 +51,58 @@
         </div>
     </div>
 </div>
+
+
+<div class="row">
+    <div class="col s12 m4">
+        <div class="card animated slideInUp">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="images/foodplaceholder.jpeg" id="picplaceholder4">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4" id="title4">Hallmark Of Burgers<i class="mdi-navigation-more-vert right"></i></span>
+                <p><a href="http://www.foodandwine.com/slideshows/burger-recipes" target="_blank" id="linktorecipe4">Make This!</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4" id="title4"><i class="mdi-navigation-close right"></i></span>
+                <h5 id="publisher1"></h5>
+                <p><a href="#" target="_blank" id="publisherurl4">Visit Them!</a></p>
+                <h6 id="rank4"></h6>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m4">
+        <div class="card animated slideInUp">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="images/burritoplaceholder.jpg" id="picplaceholder5">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4" id="title5">Best Burritos<i class="mdi-navigation-more-vert right"></i></span>
+                <p><a href="http://www.tasteofhome.com/recipes/flavorful-chicken-fajitas" target="_blank" id="linktorecipe5">Make This!</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4" id="title5"><i class="mdi-navigation-close right"></i></span>
+                <h5 id="publisher2"></h5>
+                <p><a href="#" target="_blank" id="publisherurl5">Visit Them!</a></p>
+                <h6 id="rank5"></h6>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m4">
+        <div class="card animated slideInUp">
+            <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="images/pizza.jpg" id="picplaceholder6">
+            </div>
+            <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4" id="title6">Delicous Pizza<i class="mdi-navigation-more-vert right"></i></span>
+                <p><a href="http://allrecipes.com/recipes/250/main-dish/pizza/" target="_blank" id="linktorecipe6">Make This!</a></p>
+            </div>
+            <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4" id="title6"><i class="mdi-navigation-close right"></i></span>
+                <h5 id="publisher3"></h5>
+                <p><a href="#" target="_blank" id="publisherurl6">Visit Them!</a></p>
+                <h6 id="rank6"></h6>
+            </div>
+        </div>
+    </div>
+</div>
